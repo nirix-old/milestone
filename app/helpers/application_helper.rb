f13eb85d11f6dc4c9fd34b5c88aa0d73d74f8c56
Jack Polgar <nrx@nirix.net>
2013-02-17 18:47:34 +1100

    Added the show_errors_for helper for easily showing model errors.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  ##
+  # Easily display model errors.
+  #
+  # @param [Object] model The model containing errors.
+  #
+  # @return [String]
+  #
+  # @example
+  #     <%=show_errors_for @user %>
+  #
+  def show_errors_for(model)
+    render 'errors/errors_for', model: model
+  end
 end
