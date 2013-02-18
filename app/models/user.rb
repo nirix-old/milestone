d592da9154a2bdac5f69d5c7d7ad4b5481c59a6f
Jack Polgar <nrx@nirix.net>
2013-02-18 22:32:22 +1100

    Added comments to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
+#
+# Milestone
+# Copyright (C) 2012-2013 J. Polgar
+# https://github.com/nirix
+#
+# Milestone is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 only.
+#
+# Milestone is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Milestone. If not, see <http://www.gnu.org/licenses/>.
+#
+
+##
+# User model
+#
 class User < ActiveRecord::Base
   attr_accessible :username, :email, :name, :password, :password_confirmation, :password_digest
   has_secure_password
