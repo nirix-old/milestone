141af61827c894565cf5a9783e291d365cdf92d3
Jack Polgar <nrx@nirix.net>
2013-02-18 22:32:45 +1100

    Added group relation to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,8 @@ class User < ActiveRecord::Base
   attr_accessible :username, :email, :name, :password, :password_confirmation, :password_digest
   has_secure_password
 
+  belongs_to :group
+
   validates :username, :password_confirmation, :email, :name, presence: true
   validates :username, :email, uniqueness: { case_sensitive: false }, on: :create
   validates :password_digest, confirmation: true
