fb7893a7f13e469f9bf848ed83676c62c47ae638
Jack Polgar <nrx@nirix.net>
2013-05-24 06:50:39 +1000

    Update user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,22 +16,15 @@
 # along with Milestone. If not, see <http://www.gnu.org/licenses/>.
 #
 
-##
-# User model
-#
 class User < ActiveRecord::Base
-  attr_accessible :username, :email, :name, :password, :password_confirmation, :password_digest
   has_secure_password
 
-  belongs_to :group
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 
-  validates :username, :password_confirmation, :email, :name, presence: true
-  validates :username, :email, uniqueness: { case_sensitive: false }, on: :create
-  validates :password_digest, confirmation: true
+  belongs_to :model
 
-  ##
-  # Shortcut to the groups is_admin? method.
-  #
   def is_admin?
     group.is_admin?
   end
