6443bd9bc07c618a2ec23c50b0fa005f49dda7d3
Jack Polgar <nrx@nirix.net>
2013-05-24 06:45:32 +1000

    Remove migrations and tests.

--- db/migrate/20130218111220_create_groups.rb
+++
@@ -1,8 +0,0 @@
-class CreateGroups < ActiveRecord::Migration
-  def change
-    create_table :groups do |t|
-      t.string :name, null: false
-      t.boolean :is_admin, null: false, default: false
-    end
-  end
-end
