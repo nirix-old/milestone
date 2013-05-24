25c5373dbf8754862a556a85d8d976da067d66b9
Jack Polgar <nrx@nirix.net>
2013-05-24 20:20:43 +1000

    Flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,18 @@ module ApplicationHelper
     return "" if model.errors.empty?
     render "errors/error_messages", errors: model.errors.full_messages
   end
+
+  def flash_messages
+    return unless flash.count
+
+    messages = []
+
+    flash.each do |type, message|
+      type = :success if type == :notice
+      type = :error   if type == :alert
+      messages.push({ type: type, message: message })
+    end
+
+    render 'layouts/flash_messages', messages: messages
+  end
 end
