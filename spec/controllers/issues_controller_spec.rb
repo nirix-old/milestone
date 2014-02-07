39cc848e23573ad1ccb3f04986a9f53e07304eb4
Jack Polgar <jack@polgar.id.au>
2014-02-08 03:03:54 +1100

    Create project to use for issues controller test.

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe IssuesController do
   it "should list issues" do
-    get 'index'
+    project = FactoryGirl.create :project
+    get :index, project_slug: project.slug
     response.should render_template(:index)
   end
 end
