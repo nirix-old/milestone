7d685ec2cd38c6d9544aa63fd163d9540a60b32a
Jack Polgar <jack@polgar.id.au>
2015-06-24 13:06:20 +1000

    Update users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,3 @@
-#
-# Milestone
-# Copyright (C) 2012-2013 J. Polgar
-# https://github.com/nirix
-#
-# Milestone is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 3 only.
-#
-# Milestone is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Milestone. If not, see <http://www.gnu.org/licenses/>.
-#
-
 class UsersController < ApplicationController
   before_action :already_signed_in, only: [:new, :create]
 
@@ -25,6 +7,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
+    @user.usergroup_id = 2
 
     if @user.save
       flash[:success] = t(:account_created)
