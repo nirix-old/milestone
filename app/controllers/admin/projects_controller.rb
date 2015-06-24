7daeb7008841c32a37844ce776c602ca87b10c8e
Jack Polgar <jack@polgar.id.au>
2015-06-24 13:02:48 +1000

    Update project management controller.

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,24 +1,3 @@
-#
-# Milestone
-# Copyright (C) 2012-2013 J. Polgar
-# https://github.com/nirix
-#
-# Milestone is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 3 only.
-#
-# Milestone is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Milestone. If not, see <http://www.gnu.org/licenses/>.
-#
-
-##
-# Projects controller for the admin control panel.
-#
 class Admin::ProjectsController < Admin::ApplicationController
   def index
     @projects = Project.all
@@ -38,9 +17,24 @@ class Admin::ProjectsController < Admin::ApplicationController
     end
   end
 
+  def edit
+    @project = Project.find params[:id]
+  end
+
+  def update
+    @project = Project.find params[:id]
+    @project.update_attributes project_params
+
+    if @project.save
+      redirect_to admin_projects_path
+    else
+      render :new
+    end
+  end
+
   private
 
     def project_params
-      params.require(:project).permit :name, :slug, :info
+      params.require(:project).permit(:name, :slug, :description)
     end
 end
