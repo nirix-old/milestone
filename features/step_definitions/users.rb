0b25ea94b8297d8f39b93c0b29b0a12e3bdfa198
Jack Polgar <jack@polgar.id.au>
2015-06-24 15:02:42 +1000

    Remove cucumber.

--- features/step_definitions/users.rb
+++
@@ -1,7 +0,0 @@
-Given(/^I am not logged in$/) do
-  delete logout_path
-end
-
-When(/^there is a user with the username of "(.*?)" and password of "(.*?)"$/) do |username, password|
-  FactoryGirl.create :user, username: username, password: password, password_confirmation: password
-end
