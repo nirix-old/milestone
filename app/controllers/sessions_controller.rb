4615e39877e6575902ef3bb9ef28aa55276d7fae
Jack Polgar <jack@polgar.id.au>
2015-06-24 13:05:32 +1000

    Update seesions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,3 @@
-#
-# Milestone
-# Copyright (C) 2012-2013 J. Polgar
-# https://github.com/nirix
-#
-# Milestone is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 3 only.
-#
-# Milestone is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Milestone. If not, see <http://www.gnu.org/licenses/>.
-#
-
 class SessionsController < ApplicationController
   before_action :already_signed_in, only: [:new, :create]
 
@@ -23,8 +5,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params.include?(:user) and user = User.find_by(username: params[:user][:username]) and user.try(:authenticate, params[:user][:password])
-      session[:user_id] = user.id
+    @user = User.find_by username: params[:username]
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new, locals: { error: true }
