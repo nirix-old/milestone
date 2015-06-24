183dee61a99f56925440ca7efeaf05d2c3fe4a1d
Jack Polgar <jack@polgar.id.au>
2015-06-24 13:02:16 +1000

    Update admin base controller.

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,26 +1,4 @@
-#
-# Milestone
-# Copyright (C) 2012-2013 J. Polgar
-# https://github.com/nirix
-#
-# Milestone is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 3 only.
-#
-# Milestone is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Milestone. If not, see <http://www.gnu.org/licenses/>.
-#
-
-##
-# Application controller for the admin control panel.
-#
 class Admin::ApplicationController < ApplicationController
   layout 'admin'
-
-  before_filter :require_admin
+  before_action :require_admin
 end
