1959e02cff05fadf6aa2148abaab4ec83f45914c
Jack Polgar <jack@polgar.id.au>
2015-06-30 14:35:10 +1000

    Add test to redirect logged in users away from login form.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  it 'should render login form' do
+    get :new
+    expect(response).to render_template :new
+  end
+
+  it 'should not render login form' do
+    allow(controller).to receive(:current_user).and_return(create(:user))
+
+    get :new
+    expect(response).to redirect_to root_path
+  end
+
   it 'should log user in' do
     user = create(:user, password: 'password123', password_confirmation: 'password123')
     post :create, username: user.username, password: 'password123'
